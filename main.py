fd8857c2b2dd3058b81c1a2aa5d3656b8aa036e2
Egor <egor.goncharov.osia@mail.ru>
2022-03-16 12:16:21 +0300

    Write common info about video to file

--- main.py
+++ main.py
@@ -5,18 +5,33 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import requests
+import csv
 import time
+import os
 from tqdm import tqdm
 
 class Video:
 
-    def __init__(self, name: str, src: str, link: str) -> None:
+    def __init__(self, name: str, src: str, link: str, action_item: dict) -> None:
         self.name = name
         self.src = src
         self.link = link
+        self.action_item = action_item
+
+    def info(self) -> dict:
+        output = {
+                'name': self.name,
+                'src': self.src,
+                'link': self.link
+                }
+        output.update(self.action_item)
+        return output
 
 
 def download_file(url, file_name) -> str:
+    if not os.path.exists('videos'):
+        os.mkdir('videos')
+
     local_filename = f'videos/{file_name}'
     # NOTE the stream=True parameter below
     with requests.get(url, stream=True) as r:
@@ -44,16 +59,16 @@ try:
     driver.get("https://www.tiktok.com/")
     time.sleep(5)
 
-    actions = ActionChains(driver)
+    actions = ActionChains(driver, duration=250)
 
     video_collection = []
-    for i in range(40):
+    for i in range(5):
         video = driver.find_element(By.TAG_NAME, 'video')
         like_count = driver.find_elements(By.XPATH, "//strong[@data-e2e='like-count']")
         comment_count = driver.find_elements(By.XPATH, "//strong[@data-e2e='comment-count']")
         share_count = driver.find_elements(By.XPATH, "//strong[@data-e2e='share-count']")
-
         src = video.get_attribute('src')
+
         video.click()
 
         time.sleep(2)
@@ -66,13 +81,28 @@ try:
         actions.send_keys(Keys.ESCAPE)
         actions.send_keys(Keys.ARROW_DOWN)
         actions.perform()
-        video_collection.append(Video(f'tiktok_{i}.mp4', src, link))
-        time.sleep(3)
+
+        action_items = {
+                'like_count': like_count[i].text,
+                'comment_count': comment_count[i].text,
+                'share_count': share_count[i].text
+                }
+        video_collection.append(Video(f'tiktok_{i}.mp4', src, link, action_items))
+        time.sleep(8)
 
     for video in tqdm(video_collection):
         download_file(video.src, video.name)
 
     print('Download complete.')
+
+
+    with open('tiktok_info.csv', 'w', newline='') as csvfile:
+        fieldnames = ['name', 'like_count', 'comment_count', 'share_count', 'src', 'link']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for item in video_collection:
+            writer.writerow(item.info())
 
     input()
 except Exception as e:
